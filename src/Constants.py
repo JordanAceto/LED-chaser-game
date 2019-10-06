4bd4baf6987a7baee42c16778d2aa26f7212c78d
JordanAceto <jordanaceto@gmail.com>
2019-10-06 08:21:17 -0400

    add constants py file

---
+++ src/Constants.py
@@ -0,0 +1,12 @@
+
+# sample period in seconds, controls how fast the game starts out
+STARTING_SAMPLE_PERIOD = 0.3 
+
+# list of pins to use for the led strip, uses BCM numbering
+LED_PINS = [26, 21, 20, 16, 12]
+
+# pin to use for the button, uses BCM numbering
+BUTTON_PIN = 13
+
+# number of lives you start with when you start a new round
+STARTING_NUM_LIVES = 3 
